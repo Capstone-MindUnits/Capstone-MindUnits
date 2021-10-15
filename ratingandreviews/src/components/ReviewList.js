13a8c4e28b13d1cdc2709cdcb86145f265dd8519
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-10-15 20:47:05 +0100

    review list filled

--- ratingandreviews/src/components/ReviewList.js
+++ ratingandreviews/src/components/ReviewList.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import moment from 'moment'
+
 
 function ReviewList({ reviewData }) {
     console.log('reviewData', reviewData)
 
     return (
         <div className='grid gap-4 overflow-y-scroll' style={{ height: '600px' }}>
-            {reviewData && reviewData.map((review) => {
+            {reviewData && reviewData.map((review, key) => {
                 return (
-                    <div className='grid gap-3' style={{ height: '290px' }}>
+                    <div key={key} className='grid gap-3' style={{ height: '290px' }}>
                         <div className='' >
                             <div className='flex justify-between'>
                                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
@@ -32,7 +34,7 @@ function ReviewList({ reviewData }) {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                                     </svg>
-                                    <p className='mr-2'>User1234,
+                                    <p className='mr-2'>{review.reviewer_name}
                                     </p>
                                     <p>
                                         {review.date} </p>
@@ -48,19 +50,23 @@ function ReviewList({ reviewData }) {
                         <div>
                             <p>{review.body}</p>
                         </div>
-                        <p className='flex '>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" color='black' fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
-                            </svg>
-                            I recommend this product
-                        </p>
-                        <div className=''>
-                            <p className=' font-bold'>
-                                Response:
+                        {review.recommend &&
+                            <p className='flex '>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" color='black' fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                                </svg>
+                                <p>I recommend this product</p>
                             </p>
-                            <p className=''>Marzipan danish jelly beans gummy bears apple pie cheescake topping biscuit sesame snaps
-                            </p>
-                        </div>
+                        }
+                        {review.response &&
+                            <div className=''>
+                                <p className=' font-bold'>
+                                    Response:
+                                </p>
+                                <p className=''>{review.response}
+                                </p>
+                            </div>
+                        }
                         <div className="details  flex ">
                             <p className=" mr-4 text-gray-600 flex">
                                 Helpful?
